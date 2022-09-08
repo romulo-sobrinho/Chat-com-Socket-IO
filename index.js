8032ad5d5719cc0da84f3f6e3cf212cca666cea0
Romulo <romulosilva001@gmail.com>
2022-09-08 10:53:58 -0300

    Configurando socket io para receber e enviar mensagem no index.js

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const path = require('path')
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on PORT: ${PORT}`)
-})
+const socketIo = require('socket.io')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 app.use('/', express.static(path.join(__dirname, 'public')))
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`)
+})
+
+const messages = []
+
+const io = socketIo(server)
+
+io.on('connection', (socket) => {
+  console.log('New connection')
+  socket.on('new_message', (data) => {
+    messages.push(data.msg)
+
+    io.emit('update_messages', messages)
+  })
+})
+
+
