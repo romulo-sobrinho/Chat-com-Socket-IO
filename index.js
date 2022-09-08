c1142786d4794598463941c52b61449fe34b84be
Romulo <romulosilva001@gmail.com>
2022-09-08 10:09:21 -0300

    Criado app.listen e app use no index.js, criados pasta public e arquivo index.html

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const path = require('path')
 
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`)
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+app.use('/', express.static(path.join(__dirname, 'public')))
